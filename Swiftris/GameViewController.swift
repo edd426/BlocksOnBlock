eeb3ac690f69484fdabaf6f41ec849a7ccab332c
EvanDeLord <evandelord@EvanDeLords-MacBook-Pro.local>
2017-07-13 09:55:13 -0500

    The blocks now obey the laws of physics

--- Swiftris/GameViewController.swift
+++ Swiftris/GameViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 import SpriteKit
 
-class GameViewController: UIViewController {
+class GameViewController: UIViewController, SwiftrisDelegate {
 
     var scene: GameScene!
     var swiftris:Swiftris!
@@ -31,21 +31,13 @@ class GameViewController: UIViewController {
         scene.tick = didTick
         
         swiftris = Swiftris()
+        swiftris.delegate = self
         swiftris.beginGame()
         
         
         // Present the scene.
         skView.presentScene(scene)
         
-        // #14
-        scene.addPreviewShapeToScene(shape: swiftris.nextShape!){
-            self.swiftris.nextShape?.moveTo(column: StartingColumn, row: StartingRow)
-            self.scene.movePreviewShape(shape: self.swiftris.nextShape!) {
-                let nextShapes = self.swiftris.newShape()
-                self.scene.startTicking()
-                self.scene.addPreviewShapeToScene(shape: nextShapes.nextShape!) {}
-            }
-        }
         
     }
 
@@ -58,7 +50,52 @@ class GameViewController: UIViewController {
 
     // #15
     func didTick() {
-        swiftris.fallingShape?.lowerShapeByOneRow()
-        scene.redrawShape(shape: swiftris.fallingShape!, completion: {})
+        swiftris.letShapeFall()
+    }
+    
+    func nextShape() {
+        let newShapes = swiftris.newShape()
+        guard let fallingShape = newShapes.fallingShape else {
+            return
+        }
+        self.scene.addPreviewShapeToScene(shape: newShapes.nextShape!) {}
+        self.scene.movePreviewShape(shape: fallingShape) {
+        // # 16
+            self.view.isUserInteractionEnabled = true
+            self.scene.startTicking()
+        }
+    }
+    
+    func gameDidBegin(swiftris: Swiftris) {
+        // The following is false when restarting a new game
+        if swiftris.nextShape != nil && swiftris.nextShape!.blocks[0].sprite == nil {
+            scene.addPreviewShapeToScene(shape: swiftris.nextShape!) {
+                self.nextShape()
+            }
+        } else {
+            nextShape()
+        }
+    }
+    
+    func gameDidEnd(swiftris: Swiftris) {
+        view.isUserInteractionEnabled = false
+        scene.stopTicking()
+    }
+    
+    func gameDidLevelUp(swiftris: Swiftris) {
+        
+    }
+    
+    func gameShapeDidDrop(swiftris: Swiftris) {
+        
+    }
+    
+    func gameShapeDidLand(swiftris: Swiftris) {
+        scene.stopTicking()
+        nextShape()
+    }
+    
+    func gameShapeDidMove(swiftris: Swiftris) {
+        scene.redrawShape(shape: swiftris.fallingShape!) {}
     }
 }
--- Swiftris/Shape.swift
+++ Swiftris/Shape.swift
@@ -139,8 +139,37 @@ class Shape: Hashable, CustomStringConvertible {
         }
     }
     
+    //#3
+    final func rotateClockwise() {
+        let newOrientation = Orientation.rotate(orientation: orientation, clockwise: true)
+        rotateBlocks(orientation: newOrientation)
+        orientation = newOrientation
+    }
+    
+    
+    final func rotateCounterClockwise() {
+        let newOrientation = Orientation.rotate(orientation: orientation, clockwise: false)
+        rotateBlocks(orientation: newOrientation)
+        orientation = newOrientation
+    }
+    
+    
     final func lowerShapeByOneRow() {
         shiftBy(columns: 0, rows: 1)
+    }
+    
+    
+    final func raiseShapeByOneRow() {
+        shiftBy(columns: 0, rows: 1)
+    }
+    
+    
+    final func shiftRightByOneColumn() {
+        shiftBy(columns: 1, rows: 0)
+    }
+    
+    final func shiftLeftByOneColumn() {
+        shiftBy(columns:-1, rows: 0)
     }
     
     // #2
